24acd757e6d7b1990b1b4e16522b97bf33132b57
benjithorpe <benthorpe.dev@gmail.com>
2023-03-17 10:26:43 +0000

    add get route for recipe

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+@app.route("/recipe")
+def get_recipe():
+    return [{"title":"Pizza Recipe", "author":"John Doe","description":"A simple pizza recipe!!"}]
