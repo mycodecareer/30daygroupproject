73735be3e0a5f4c3ba42e87010a3147f9a69cb6e
Angelos Koliopoulos <koliosor@gmail.com>
2023-03-19 14:39:24 +0200

    added react router (#36)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,21 @@
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
