e005d2817adefcd631ae00f24f30996129cf0883
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-03-24 17:14:17 +0200

    Added POST method which recieves three inputs from form: title, description and author. Returns message if any of them missing otherwise returns dictionary with them

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,27 @@ def hello():
     return "<h1>Hello, World!</h1>"
 
 
-@app.route("/recipe", methods=["GET"])
+@app.route("/recipe", methods=["GET", "POST"])
 def recipe():
-    return {"title": "Pizza", "description": "A delicious pizza!", "author": "John Doe"}
+    if request.method == "POST":
+        # Lets store data recieved from form in a dictionary
+        record = {}
+        # Append information only if it's present
+        if not request.form.get("title"):
+            return "Title is missing"
+        else:
+            record["title"] = request.form.get("title")
+
+        if not request.form.get("description"):
+            return "Description is missing"
+        else:
+            record["description"] = request.form.get("description")
+
+        if not request.form.get("author"):
+            return "Who is author by the way"
+        else:
+            record["author"] = request.form.get("author")
+            
+        return record
+    else:
+        return {"title": "Pizza", "description": "A delicious pizza!", "author": "John Doe"}
