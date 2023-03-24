9d2dc57bf1097f26556c93f380d4f36d23c5a325
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-03-24 18:32:09 +0200

    Added reactstrap dependency & simple form page for recipes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,15 @@
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import Container from "react-bootstrap/Container";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MyForm from "./Form";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Container>Hello world!</Container>,
+    element: <MyForm />,
   },
 ]);
 
