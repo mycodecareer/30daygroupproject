9fa0ebf1a5427ab113856ca09a79260d0d6b02f9
Denis <103317598+DAPotapov@users.noreply.github.com>
2023-03-30 18:00:42 +0300

    backend: Added recipe POST method handler  (#45)
    * Added POST method which recieves three inputs from form: title, description and author.
    Returns message if any of them missing otherwise returns dictionary with them
    
    * Added tests for POST
    
    * Reformatted after linters
    
    * POST tests refactored to be one test. Absense of each of three parameters tested.
    
    * POST test function name changed

--- backend/test_app.py
+++ backend/test_app.py
@@ -25,3 +25,29 @@ def test_get_recipe(client):
         b'{"author":"John Doe","description":"A delicious pizza!","title":"Pizza"}\n'
         in resp.data
     )
+
+
+def test_post_recipe(client):
+    resp = client.post("/recipe")
+    assert b"Title is missing" in resp.data
+
+    resp = client.post("/recipe", data={"title": "Pelmeny", "author": "babushka"})
+    assert b"Description is missing" in resp.data
+
+    resp = client.post(
+        "/recipe", data={"title": "Pelmeny", "description": "Meatballs in dough"}
+    )
+    assert b"Who is author by the way?" in resp.data
+
+    resp = client.post(
+        "/recipe",
+        data={
+            "title": "Pelmeny",
+            "description": "Meatballs in dough",
+            "author": "babushka",
+        },
+    )
+    assert (
+        b'{"author":"babushka","description":"Meatballs in dough","title":"Pelmeny"}\n'
+        in resp.data
+    )
