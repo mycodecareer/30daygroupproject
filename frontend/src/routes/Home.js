4533c667093667b2a26088bd128e4aec40261573
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-04-09 12:25:14 +0300

    Added environment varible check to home route

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -2,7 +2,7 @@ import CommonNavbar from "../components/navbar";
 
 const Home = () => {
   return (
-    <div>
+    <div onLoad={console.log(process.env.REACT_APP_TEST_ENV)}>
       <CommonNavbar />
       <h1>Welcome to Home</h1>
     </div>
